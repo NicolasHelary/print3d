02ce64f6e0c19aad1e39988ae2dfb3d5b39d3b52
Nicolas Helary <helary.nicolas@gmail.com>
2022-06-15 17:37:23 +0200

    ajout des seeds valides

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,6 @@ class Offer < ApplicationRecord
   belongs_to :demand
   belongs_to :printer, class_name: "User"
 
-  validates :price, presence: true, null: false
-  validates :status, presence: true, null: false, default: 1
+  validates :price, presence: true
+  validates :status, presence: true
 end
