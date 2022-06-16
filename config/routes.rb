4bd87d39156d831c0fdd572216592ad1dea8d6c2
brice costantini <famillecostantini@hotmail.com>
2022-06-16 15:30:13 +0200

    controller view route product

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'demands#index'
+  resources :products, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
