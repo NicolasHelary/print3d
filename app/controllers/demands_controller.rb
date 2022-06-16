0b600acbe04df8d0bdb1e5ade3586688007f0fec
Nicolas Helary <helary.nicolas@gmail.com>
2022-06-16 17:16:01 +0200

    sauvegarde intermédiaire

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -1,5 +1,5 @@
 class DemandsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new]
   before_action :set_demand, only: [:show]
 
   def index
@@ -9,6 +9,25 @@ class DemandsController < ApplicationController
   def show
   end
 
+  def new
+    @demand = Demand.new
+    @user = current_user
+    @product = Product.find(params[:product_id])
+  end
+
+  def create
+    @demand = Demand.new
+    @user = User.find(params[:user_id])
+    @product = Product.find(params[:product_id])
+    @demand.user = @user
+    @demand.product = @product
+    if @demand.save
+      redirect_to profile_path
+      flash[:alert] = "You successfully made a print demand for #{@product}"
+    else
+      render :new
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'demands#index'
-  resources :demands
+  resources :products, only: %i[index show] do
+    resources :demands
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
