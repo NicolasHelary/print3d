7b5a2b4ba39ef190013900a03013e82d4533bd93
Romualdo Delain <romualdo.delain@outlook.com>
2022-06-21 18:14:22 +0200

    adding picture to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :product_categories
   has_many :categories, through: :product_categories
   has_many :demands, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
