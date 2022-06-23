f1c88897be5baec3b7e06af8c1901240a46b73b4
Nicolas Helary <helary.nicolas@gmail.com>
2022-06-23 09:58:24 +0200

    ajout bio to users et autre chose mais j'ai oublié

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_21_085557) do
+ActiveRecord::Schema.define(version: 2022_06_23_075512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 2022_06_21_085557) do
     t.boolean "printer", default: false
     t.float "latitude"
     t.float "longitude"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
